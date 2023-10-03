446bfe61b9fbc2799e3650aefb5823f9355fdd8a
tamani phiri <tamanigabriel0@gmail.com>
2023-10-03 19:55:25 +0200

    changes

--- Models/Brand.cs
+++ Models/Brand.cs
@@ -5,4 +5,8 @@ public class Brand
     public int ID { get; set; }
     
     public string Name { get; set; }
+    
+    public string Category { get; set; }
+    
+    public int isActive{get; set; }
 }

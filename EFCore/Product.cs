7456497931b43c86c52a42d8f29ec27164497dce
tamani phiri <tamanigabriel0@gmail.com>
2023-10-04 09:09:26 +0200

    changes

--- EFCore/Product.cs
+++ EFCore/Product.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace dotNetOrigins.EFCore;
@@ -5,5 +6,8 @@ namespace dotNetOrigins.EFCore;
 [Table("product")]
 public class Product
 {
+    [Key,Required]
     public int id { get; set; }
+    
+    public string name { get; set; }
 }

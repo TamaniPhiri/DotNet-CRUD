a6e7390ae863449106166a2e19c245e6ff9d8b99
tamani phiri <tamanigabriel0@gmail.com>
2023-10-04 09:12:15 +0200

    changes

--- EFCore/Order.cs
+++ EFCore/Order.cs
@@ -1,6 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace dotNetOrigins.EFCore;
 
+[Table("order")]
 public class Order
 {
-    
+    [Key, Required] public int id { get; set; };
 }

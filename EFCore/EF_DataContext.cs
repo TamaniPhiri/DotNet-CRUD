8c6b3d39970369a0f97ee87f3c425382c07d287e
tamani phiri <tamanigabriel0@gmail.com>
2023-10-04 09:39:50 +0200

    changes

--- EFCore/EF_DataContext.cs
+++ EFCore/EF_DataContext.cs
@@ -2,10 +2,15 @@ using Microsoft.EntityFrameworkCore;
 
 namespace dotNetOrigins.EFCore;
 
-public class EF_DataContext
+public class EF_DataContext:DbContext
 {
     public EF_DataContext(DbContextOptions<EF_DataContext> options):base(options)
     {
         
+    }
+
+    public override void onModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.UseSerialColumns();
     }
 }
